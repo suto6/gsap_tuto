689650b7037f7393caaa7b32d8b56eedc07dc52e
Suto <sutapacreator@gmail.com>
2025-11-19 23:05:16 +0530

    yoyo

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-gsap.to("#box", {
-    x:1000,
-    duration: 13,
+gsap.from("#box", {
+    x:1050,
+    duration: 3,
     delay:2,
+    rotate:"+=360",
+    backgroundColor:"#000000ff",
+    borderRadius:"50%",
+    // ease: "elastic.out(1, 0.4)",
+    scale:"-=1",
+    repeat:-1, //infinite time
+    yoyo:true,
+
 })
 gsap.to("#box2", {
-    y:1000,
-    duration: 13,
+    x:1000,
+    duration: 3,
     delay:2,
+    borderRadius:"50%",
+    backgroundColor:"#002262ff",
+    // ease: "elastic.out(1, 0.4)",
+    rotate:"+=360",
+    scale:"-=1",
+    repeat:-1,
+    yoyo:true,
+
+
+
+
+})
+
+gsap.from("h2", {
+    // color:"red",
+    opacity:0,
+    duration: 2,
+    y:20,
+    delay:1,
+    stagger:0.5,
+    ease: "bounce"
 })
