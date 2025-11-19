53776c5bf0b50c0b5599675f93b2fd2e86cfaf40
Suto <sutapacreator@gmail.com>
2025-11-20 01:58:40 +0530

    scrollTrigger

--- script.js
+++ script.js
@@ -44,10 +44,89 @@ tl2.from("h4", {
     stagger:0.5,
     ease: "bounce"
 })
-tl2.from("h1", {
-    y: -700,
+// tl2.from("h1", {
+//     y: -700,
+//     duration: 2,
+//     delay:0.5,
+//     opacity:0,
+//     scale:0.8,
+// })
+
+gsap.from("#page1 #b", {
+    scale: 0,
+    delay:1,
     duration: 2,
-    delay:0.5,
+    rotate:"+=360",
+    ease: "power4.inOut"
+})
+gsap.from("#page2 h1", {
+    scale: 0,
+    // delay:1,
+    duration: 2,
+    // rotate:"+=360",
+    // ease: "power4.inOut",
     opacity:0,
-    scale:0.8,
+    x:100,
+    stagger:0.5,
+    scrollTrigger: {
+        trigger: "#page2 h1",
+        scroller:"body",
+        markers:true,
+        start:"top 60%",
+    }
+})
+gsap.from("#page2 h3", {
+    scale: 0,
+    // delay:1,
+    duration: 2,
+    // rotate:"+=360",
+    // ease: "power4.inOut",
+    opacity:0,
+    x:-100,
+    stagger:0.5,
+    scrollTrigger: {
+        trigger: "#page2 h3",
+        scroller:"body",
+        markers:true,
+        start:"top 50%",
+    }
+})
+gsap.from("#page2 #b", {
+    scale: 0,
+    // delay:1,
+    duration: 2,
+    rotate:"+=760",
+    // ease: "power4.inOut",
+    opacity:0,
+    // x:-100,
+    // stagger:0.5,
+    scrollTrigger: {
+        trigger: "#page2 #b",
+        scroller:"body",
+        markers:true,
+        start:"top 40%",
+        end:"top 20%",
+        scrub: 3,
+        pin: true,
+    }
+})
+gsap.from("#page3 #b", {
+    scale: 0,
+    delay:1,
+    duration: 2,
+    rotate:"+=360",
+    ease: "power4.inOut"
+})
+gsap.to("#home2 h1", {
+    transform:"translateX(-70%)",
+    duration: 2,
+    scrollTrigger:{
+        trigger:"#home2",
+        scroller:"body",
+        markers:true,
+        start:"top 0%",
+        scrub: 3,
+        pin: true,
+        end:"top -150%"
+    }
 })
